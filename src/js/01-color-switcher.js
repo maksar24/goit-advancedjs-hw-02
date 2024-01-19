ccc233386bcc13468b8916468527a183744fe81c
Leskovets Maksym <leskovecmaksim@gmail.com>
2024-01-19 22:48:00 +0100

    finished color-swithcer

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const elements = {
+    body: document.querySelector("body"),
+    btnStart: document.querySelector("[data-start]"),
+    btnStop: document.querySelector("[data-stop]"),
+};
+let timerId = null;
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
+
+function changeColor() {
+    timerId = setInterval(() => {
+        let color = getRandomHexColor();
+        elements.body.style.backgroundColor = color;
+    }, 1000);
+    elements.btnStart.disabled = true;
+};
+
+function stopChangeColor() {
+    clearInterval(timerId);
+    elements.btnStart.disabled = false;
+};
+
+elements.btnStart.addEventListener("click", changeColor);
+elements.btnStop.addEventListener("click", stopChangeColor);
